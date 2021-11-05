d139ecbbd1a331821b49bdaaf6d5032d95d52ac8
Kristof1992 <kristof92selecz@gmail.com>
2021-11-05 14:28:52 +0000

    Usage of: Recycler View, Gradient

---
+++ 3_BucketList_App/app/src/main/java/com/kristof/bucketlistapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.kristof.bucketlistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
+        CardView thingsToDoCard = findViewById(R.id.card_view_things_to_do);
+        CardView placesToGoCard = findViewById(R.id.places_to_go);
+
+        thingsToDoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        placesToGoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
